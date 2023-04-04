fe17337fe9293ea71024dc9b9e0745b8136ac211
clearlybaffled <clearlybaffled120@gmail.com>
2023-04-03 23:23:28 -0400

    Add set fail flag in setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
+set -e
 GIT="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 REPO="git@github.com:clearlybaffled/dotfiles.git"
 
@@ -10,6 +11,8 @@ $GIT config status.showUntrackedFiles no
 $GIT config --local include.path $HOME/git/.gitconfig
 $GIT submodule update --init --remote
 $GIT remote set-url origin $REPO
+
+set +e
 
 source $HOME/.bashrc
 
